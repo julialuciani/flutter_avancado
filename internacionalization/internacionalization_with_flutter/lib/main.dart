1948ac516d51a38bd382a502eab64a648d08a684
Júlia <Jlia.Oliveira@proway.treina>
2022-08-10 16:48:09 -0300

    fixing

--- internacionalization/internacionalization_with_flutter/lib/main.dart
+++ internacionalization/internacionalization_with_flutter/lib/main.dart
@@ -2,27 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-
 void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
 final languageProvider = StateNotifierProvider<LanguageNotifier, Locale>(
- (ref) => LanguageNotifier(),
+  (ref) => LanguageNotifier(),
 );
 
-class LanguageNotifier extends StateNotifier<Locale>{
-  LanguageNotifier() : super (const Locale('pt', 'BR'));
+class LanguageNotifier extends StateNotifier<Locale> {
+  LanguageNotifier() : super(const Locale('pt', 'BR'));
 
-  void changeLanguage(){
-  if (state == const Locale('en', '')){
-    state = const Locale('pt', 'BR');
-  } else{
-    state = const Locale('en','');
+  void changeLanguage() {
+    if (state == const Locale('en', '')) {
+      state = const Locale('pt', 'BR');
+    } else {
+      state = const Locale('en', '');
+    }
   }
-
-  }
-
 }
 
 class MyApp extends HookConsumerWidget {
@@ -30,7 +27,7 @@ class MyApp extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-     final languages = ref.watch(languageProvider);
+    final languages = ref.watch(languageProvider);
 
     return MaterialApp(
       locale: languages,
@@ -47,21 +44,29 @@ class MyApp extends HookConsumerWidget {
 }
 
 class MyHomePage extends HookConsumerWidget {
-const MyHomePage({Key? key, }) : super(key: key);
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Internacionalization'),
+        title: const Text(
+          'INTL',
+          style: TextStyle(letterSpacing: 3),
+        ),
+        centerTitle: true,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(AppLocalizations.of(context)!.helloWorld),
-            Text(AppLocalizations.of(context)!.myName),
+            Text(
+              AppLocalizations.of(context)!.helloWorld,
+              style: const TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+            ),
+            Text(AppLocalizations.of(context)!.myName, style: const TextStyle(fontSize: 15),),
           ],
         ),
       ),
@@ -95,9 +100,3 @@ const MyHomePage({Key? key, }) : super(key: key);
     );
   }
 }
-
-
-
- 
-
-    

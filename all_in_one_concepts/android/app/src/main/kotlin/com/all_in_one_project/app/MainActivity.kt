ebfa93682ace2d36b617b621da7f70f7215bb848
Júlia <Jlia.Oliveira@proway.treina>
2022-08-16 08:35:10 -0300

    generating appbundle

---
+++ all_in_one_concepts/android/app/src/main/kotlin/com/all_in_one_project/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.all_in_one_project.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ http_rest_api/get_api_movies/android/app/src/main/kotlin/com/api_movies/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.api_movies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

a3e7265797933c00c38087fd32c8901269606906
Júlia <Jlia.Oliveira@proway.treina>
2022-08-18 13:19:07 -0300

    integration test

--- testes/unit_test_flutter/test/widget_test/heart_test.dart
+++ testes/unit_test_flutter/test/widget_test/heart_test.dart
@@ -1,59 +1,10 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
-
-// class Heart extends StatefulWidget {
-//   const Heart({Key? key}) : super(key: key);
-
-//   @override
-//   State<Heart> createState() => _HeartState();
-// }
-
-// class _HeartState extends State<Heart> {
-//   IconData icon = Icons.favorite_border;
-//   double size = 100;
-//   Color color = Colors.pink.shade300;
-
-
-//   changeIcon() {
-//     if (icon == Icons.favorite_border) {
-//       icon = Icons.favorite;
-//       size = 200;
-//       color = Colors.pink;
-//     } else {
-//       icon = Icons.favorite_border;
-//       size = 100;
-//       color = Colors.pink.shade300;
-      
-//     }
-
-//     setState(() {});
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: InkWell(
-//         onTap: () {
-//           changeIcon();
-//         },
-//         key: const Key('HeartButton'),
-//         child: Icon(icon, size: size, color: color, key: const Key('HeartIcon'),),
-//       ),
-//     );
-//   }
-// }
-
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:unit_test_flutter/home_page.dart';
 
 Widget makeTestable(Widget widget) => MaterialApp(home: widget);
-void main(){
-
-  testWidgets('Testing the heart animation', (WidgetTester tester) async{
-
+void main() {
+  testWidgets('Testing the heart animation', (WidgetTester tester) async {
     await tester.pumpWidget(makeTestable(const HomePage()));
 
     final Finder iconFinder = find.byKey(const Key('IconHeart'));
@@ -81,38 +32,5 @@ void main(){
     expect(iconAfter.color, Colors.pink);
 
     expect(iconAfter.icon, Icons.favorite);
-
   });
-
-  //   testWidgets('Testing the heart animation', (WidgetTester tester) async{
-
-  //   await tester.pumpWidget(makeTestable(const Heart()));
-
-  //   final Finder iconFinder = find.byKey(const Key('HeartIcon'));
-
-  //   expect(iconFinder, findsOneWidget);
-
-  //   final Icon icon = tester.widget(iconFinder);
-
-  //   expect(icon.color, Colors.pink.shade300);
-
-  //   expect(icon.size, 100);
-
-  //   expect(icon.icon, Icons.favorite_border);
-
-  //   final tapFinder = find.byKey(const Key('HeartButton'));
-
-  //   await tester.tap(tapFinder);
-
-  //   await tester.pumpAndSettle();
-
-  //   final Icon iconAfter = tester.widget(iconFinder);
-
-  //   expect(iconAfter.size, 200);
-
-  //   expect(iconAfter.color, Colors.pink);
-
-  //   expect(iconAfter.icon, Icons.favorite);
-
-  // });
 }
